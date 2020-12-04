e47ad5d41de23c81017e40bd8dcfe5d20647a960
kevin.yu <kevin.yu256@mail.hamastar.com.tw>
2020-12-04 10:58:51 +0800

    feat: add connection

---
+++ Controllers/Home/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggerTest.Controllers.Home
+{
+    [Route("api/Manage/[controller]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// List
+        /// </summary>
+        /// <returns>List</returns>
+        [HttpGet]
+        [Route("live")]
+        public bool Live()
+        {
+            return true;
+        }
+    }
+}
